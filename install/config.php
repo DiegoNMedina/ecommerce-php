6bf4c0b1416ac1daf5570486ac29e13c60e8f654
DiegoNMedina <dieego.meedina@outlook.es>
2025-07-24 23:11:22 -0600

    update: update config.php

--- install/config.php
+++ install/config.php
@@ -2,12 +2,13 @@
 
 namespace Install;
 
-class Config {
+class Config
+{
     // Database configuration
-    const DB_HOST = 'localhost';
-    const DB_NAME = 'ecommerce_computers';
+    const DB_HOST = '127.0.0.1:8889'; // MAMP MySQL port
+    const DB_NAME = 'ecommerce_php';
     const DB_USER = 'root';
-    const DB_PASS = 'root'; // Default MAMP password
+    const DB_PASS = 'root'; // MAMP default password
     const DB_CHARSET = 'utf8mb4';
 
     // Application configuration
